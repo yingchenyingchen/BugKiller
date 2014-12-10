de3d7be23332d81c6c7d9bef1a44bbec9df9d414
Johnathan Bostrom <johnbos@ksu.edu>
2014-12-09 21:10:20 -0600

    all merged

--- Assets/Resources/ProjectAssets/Scripts/FPSInputController.cs
+++ Assets/Resources/ProjectAssets/Scripts/FPSInputController.cs
@@ -9,11 +9,18 @@ using System.Collections.Generic;
 public class FPSInputController : MonoBehaviour
 {
     private CharacterMotor motor;
-
+    float walkSpeed = 7f;
+    float crchSpeed = 3f;
+    float runSpeed = 20f;
+   
+    private CharacterController ch;
+    private float height;
     // Use this for initialization
     void Awake()
     {
         motor = GetComponent<CharacterMotor>();
+        ch = GetComponent<CharacterController> ();
+        height = ch.height;
     }
 
     // Update is called once per frame
@@ -21,7 +28,7 @@ public class FPSInputController : MonoBehaviour
     {
         // Get the input vector from kayboard or analog stick
         Vector3 directionVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
+        Crouch ();
         if (directionVector != Vector3.zero)
         {
             // Get the length of the directon vector and then normalize it
@@ -43,5 +50,21 @@ public class FPSInputController : MonoBehaviour
         // Apply the direction to the CharacterMotor
         motor.inputMoveDirection = transform.rotation * directionVector;
         motor.inputJump = Input.GetButton("Jump");
+    }
+        void Crouch()
+    {
+        float h = height;
+        float speed = walkSpeed;
+        
+        if (ch.isGrounded && Input.GetKey (KeyCode.LeftShift)) {
+            speed = runSpeed;
+        }
+        if (Input.GetKey (KeyCode.C)) {
+            h = .5f*height;
+            speed = crchSpeed;
+        }
+        motor.movement.maxForwardSpeed = speed;
+        float lastHeight = ch.height;
+        ch.height = Mathf.Lerp (ch.height, h, 5 * Time.deltaTime);
     }
 }
