aeff42f78c1dd66096027df3fb0d1a1196e07732
Ian Speer <euhnspeer@gmail.com>
2015-02-01 23:10:46 -0600

    Moved and deleted unused scripts. Began documentation.

---
+++ Assets/Resources/ProjectAssets/Scripts/Unused Scripts/ObjectGrab(unused).cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectGrab : MonoBehaviour {
+	private bool handsFull = false;
+	Rigidbody objectHeld;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetMouseButtonUp (0))
+		{
+			if(handsFull)
+				dropObjects ();
+			return;
+		}
+		
+		if (!Input.GetMouseButtonDown (0))
+			return;
+		
+		RaycastHit hit = new RaycastHit ();
+		
+		if (!Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit, 500)) {
+			return;
+		}
+		Rigidbody body = hit.rigidbody;
+		if (!body || body.isKinematic) {
+			return;
+		}
+		
+		grabObject (body);
+	}
+	
+	void grabObject(Rigidbody body)
+	{
+		objectHeld = body;
+		handsFull = true;
+		body.useGravity = false;
+		body.transform.parent = Camera.main.transform;
+		body.isKinematic = true;
+	}
+	
+	void dropObjects()
+	{
+		print ("DROP");
+		handsFull = false;
+		objectHeld.useGravity = true;
+		objectHeld.transform.parent = null;
+		objectHeld.isKinematic = false;
+	}
+}
