25733852e6b02261d0742b7f4c4ac6d9a4da7143
Ian Speer <euhnspeer@gmail.com>
2015-02-03 00:24:24 -0600

    Further documented and organized code

---
+++ Assets/Resources/ProjectAssets/Scripts/Bug Related/Block_Move.cs
@@ -0,0 +1,34 @@
+﻿/// <summary>
+/// Part of Johnothan's code toward the end. 
+/// </summary>
+using UnityEngine;
+using System.Collections;
+
+public class Block_Move : MonoBehaviour {
+	
+	public GameObject otherBlock;
+	Vector3 vector;
+	public float maxDistance;
+	
+	// Use this for initialization
+	void Start () {
+		//otherBlock = GameObject.FindWithTag("BlockToOrbitAround");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// moves the block up and forward
+		//transform.Translate (Vector3.forward * Time.deltaTime);
+		//transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+		vector = otherBlock.transform.position - gameObject.transform.position;
+		if (vector.magnitude > maxDistance)
+			transform.LookAt (otherBlock.transform.localPosition);
+		else {
+			if (Random.value < 0.6)	
+				transform.Rotate (0, Random.Range (-15, 25), 0);
+		}
+		transform.Translate (Vector3.forward * Time.deltaTime);
+		
+		//transform.RotateAround(otherBlock.transform.position, Vector3.up, 20 * Time.deltaTime);
+	}
+}
---
+++ Assets/Resources/ProjectAssets/Scripts/Equipment/EquipmentHandler.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EquipmentHandler : MonoBehaviour {
+
+	public Equipment EquipmentHeldRight;
+	public Equipment EquipmentHeldLeft;
+	public GameObject RightHand;
+	public GameObject LeftHand;
+	public Equipment HandModelLeft;
+	public Equipment HandModelRight;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (EquipmentHeldLeft && Input.GetMouseButtonDown(0))
+			EquipmentHeldLeft.Activate (KeyCode.Mouse0);
+		if (EquipmentHeldRight && Input.GetMouseButtonDown(1))
+			EquipmentHeldRight.Activate (KeyCode.Mouse1);
+
+	}
+
+	public void EquipRight(Equipment equipment)
+	{
+		if(checkEquipped(equipment, "right"))
+			return;
+		if (equipment.gameObject.name == "LHand")
+			return;
+
+		UnequipRight ();
+		EquipmentHeldRight = (Equipment)Instantiate(equipment);
+		EquipmentHeldRight.transform.position = RightHand.transform.position;
+		EquipmentHeldRight.transform.rotation = RightHand.transform.rotation;
+		EquipmentHeldRight.transform.parent = RightHand.transform;
+	}
+
+	public void EquipLeft(Equipment equipment)
+	{
+		if(checkEquipped(equipment, "left"))
+			   return;
+		if (equipment.gameObject.name == "RHand")
+			return;
+		UnequipLeft ();
+		EquipmentHeldLeft = (Equipment)Instantiate(equipment);
+		EquipmentHeldLeft.transform.position = LeftHand.transform.position;
+		EquipmentHeldLeft.transform.rotation = LeftHand.transform.rotation;
+		EquipmentHeldLeft.transform.parent = LeftHand.transform;
+	}
+
+	//Deals with any equipped items that are a clone of param equipment
+	//if such equipment is equipped to the hand that we are trying to equip to, return true
+	//otherwise return false and if such equipment is equipped to the other hand, unequip it.
+	private bool checkEquipped(Equipment equipment, string hand)
+	{
+		Equipment thisHand, otherHand;
+		string name = equipment.name + "(Clone)";
+		System.Action unequip;
+		if (hand == "right")
+		{
+			otherHand = EquipmentHeldLeft;
+			thisHand = EquipmentHeldRight;
+			unequip = () => UnequipLeft();
+		} 
+		else
+		{
+			otherHand = EquipmentHeldRight;
+			thisHand = EquipmentHeldLeft;
+			unequip = () =>  UnequipRight();
+		}
+		if (thisHand && thisHand.name == name) //if there is something equipped in hand and its name is the same 
+		{
+			return true;
+		} 
+		else if (otherHand && otherHand.name == name)
+			unequip ();
+		return false;
+	}
+
+	public void UnequipRight()
+	{
+		if(EquipmentHeldRight)
+			DestroyImmediate(EquipmentHeldRight.gameObject);
+		EquipmentHeldRight = null;
+	}
+
+	public void UnequipLeft()
+	{
+		if(EquipmentHeldLeft)
+			DestroyImmediate(EquipmentHeldLeft.gameObject);
+		EquipmentHeldLeft = null;
+	}
+}
